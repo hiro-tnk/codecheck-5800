137000b570276c17b49b1c45d34355341c02d0f2
code-project <dev@code-check.io>
2017-01-28 20:04:56 +0000

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from xml.dom import minidom
+import urllib.request
+import urllib
+def main(argv):
+  baseurl='http://54.92.123.84/search?ackey=869388c0968ae503614699f99e09d960f9ad3e12&q=Body:'
+  maxnum=0
+  for v in argv:
+      print(argv)
+      with urllib.request.urlopen(baseurl+urllib.parse.quote(v)) as req:
+          doc = minidom.parseString(req.read())
+          num = int(doc.getElementsByTagName('result')[0].getAttribute('numFound'))
+          if maxnum<=num:
+              maxnum=num
+              maxv=v
+
+  print('{{name: {0}, count: {1}}}'.format(maxv,maxnum))
+
